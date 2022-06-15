a95a9d7dc95053b2be9f355c414ce810922f5797
Antonio Emerson <emerson.nessa@gmail.com>
2022-06-15 15:35:19 -0300

    criação de classes no typescript

---
+++ exercicio-classes/build/class.js
@@ -0,0 +1,50 @@
+"use strict";
+//Classes
+class LeagueNBA {
+    //Não tem necessidade de definir as propriedades aqui:
+    // public club: string, 
+    // public players: string, 
+    // public city: string,
+    // private avaliable: boolean
+    constructor(club, players, city, avaliable, //Pode ser acessada somente pela própria classe.
+    wage) {
+        this.club = club;
+        this.players = players;
+        this.city = city;
+        this.avaliable = avaliable;
+        this.wage = wage;
+        //Não há necessidade de inicializar as propriedades aqui:
+        // this.club = club;
+        // this.players = players;
+        // this.city = city;
+        // this.avaliable = avaliable;
+    }
+    retornaDados() {
+        //Função para poder acessar os dados 'Private' e 'Protected'
+        return `Podera jogar a proxima partida...... ${this.avaliable}
+      Quanto ele vai ganhar........ ${this.wage}`;
+    }
+}
+const team1 = new LeagueNBA('Celtic', 'Jason Tatum', 'Boston', true, 500);
+const team2 = new LeagueNBA('Warriors', 'Steph Curry', 'San Francisco', true, 800);
+console.log(team1.club, team1.players, team1.city);
+console.log(team2.club, team2.players, team2.city);
+console.log(`O principal jogador do Celtics é ${team1.players}`);
+console.log(`O principal jogador do Warriors é ${team2.players}`);
+console.log(team1.retornaDados());
+//Exemplo de classe 'filha'
+class Color extends LeagueNBA {
+    constructor(colorsShirt, club, players, city, avaliable, wage) {
+        super(club, players, city, avaliable, wage);
+        this.colorsShirt = colorsShirt;
+        this.colorsShirt = colorsShirt; //ao utilizar protected, podemos acessar por classes
+    }
+    retornaDados2() {
+        //Função para poder acessar os dados 'Private' e 'Protected'
+        return `A cor da camisa é..... ${this.colorsShirt}
+    Quanto ele vai ganhar........ ${this.wage}`;
+    }
+}
+const team3 = new Color('Verde', 'Celtic', 'Al Horford', 'Boston', true, 500);
+console.log(team3);
+console.log(team3.retornaDados2());
